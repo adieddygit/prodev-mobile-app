fd37e7d9d481621ddc48609c0ce6956980dad10c
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2026-01-15 06:28:46 +0000

    Navigation using Stack Navigation Patterns

--- prodev-mobile-app-0x04/app/signin.tsx
+++ prodev-mobile-app-0x04/app/signin.tsx
@@ -9,7 +9,7 @@ export default function Index() {
       <SafeAreaView style={styles.container}>
         <View style={styles.formGroup}>
           <Ionicons name="arrow-back" size={25} />
-          <Image source={require('assets/images/logo.png')}/>
+          <Image source={require('@/assets/images/logo.png')}/>
         </View>
         <Text style={styles.titleText}>Sign in to your</Text>
         <Text style={styles.titleText}>Account</Text>
